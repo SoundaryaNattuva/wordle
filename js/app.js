9ff6d72c20623c208eaadd1a53c06c22b3b087c8
SoundaryaNattuva <soundaryanatts@gmail.com>
2023-12-22 15:09:54 -0500

    create delete fxn

--- js/app.js
+++ js/app.js
@@ -3,21 +3,28 @@
 
 /*-------------------------------- Variables --------------------------------*/
 let activeRow = 0
+let activeCol = 0
 
 /*------------------------ Cached Element References ------------------------*/
 const keysList = document.querySelectorAll('.key')
-const tileList = document.querySelectorAll('.tile') 
+const tileList = document.querySelectorAll('.tile')
+const tileListR0 = document.querySelectorAll('.row0Tile')
 const tileListR1 = document.querySelectorAll('.row1Tile') 
 const tileListR2 = document.querySelectorAll('.row2Tile') 
 const tileListR3 = document.querySelectorAll('.row3Tile') 
 const tileListR4 = document.querySelectorAll('.row4Tile') 
 const tileListR5 = document.querySelectorAll('.row5Tile') 
-const tileListR6 = document.querySelectorAll('.row6Tile')
+
+const deleteKey = document.querySelector('#delete')
+const enterKey = document.querySelector('#enter')
 
 /*----------------------------- Event Listeners -----------------------------*/
 keysList.forEach((key) => {
     key.addEventListener('click',letterClick)
 })
+
+deleteKey.addEventListener("click", deleteLetter)
+
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -30,28 +37,33 @@ function letterClick(evt){
 
 function updateBoardTile(letterChosen){
     if (activeRow === 0){
-        checkRow(letterChosen, tileListR1 )
+        checkRow(letterChosen, tileListR0)
     } else if (activeRow === 1){
-        checkRow(letterChosen, tileListR2)
+        checkRow(letterChosen, tileListR1)
     } else if (activeRow === 2){
-        checkRow(letterChosen, tileListR3)
+        checkRow(letterChosen, tileListR2)
     } else if (activeRow === 3){
-        checkRow(letterChosen, tileListR4)
+        checkRow(letterChosen, tileListR3)
     } else if (activeRow === 4){
-        checkRow(letterChosen, tileListR5)
+        checkRow(letterChosen, tileListR4)
     } else if (activeRow === 5){
-        checkRow(letterChosen, tileListR6)
+        checkRow(letterChosen, tileListR5)
     }
 }
 
 function checkRow(letterChosen, tileListRow){
     for (let i=0; i < tileListRow.length; i++){
-        if(tileListR1[i].textContent === ''){
-            tileListR1[i].textContent = letterChosen
+        if(tileListRow[i].textContent === ''){
+            tileListRow[i].textContent = letterChosen
+            activeCol += 1
             return
-        } else if (tileListR1[i].textContent != ''){
+        } else if (tileListRow[i].textContent != ''){
             continue
         } 
     }
 }
 
+function deleteLetter(){
+    document.querySelector(`#R${activeRow}C${activeCol-1}`).textContent = ''
+    activeCol -= 1
+}
